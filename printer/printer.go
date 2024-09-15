e33c59be8ce42ee2575c5dbd534b3ec3b20cffac
Rickie Karp <rickie.karp@yandex.com>
2024-09-15 11:49:55 +0200

    Add temporary code to list all files and their file paths

--- printer/printer.go
+++ printer/printer.go
@@ -3,6 +3,7 @@ package printer
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"git.rickiekarp.net/rickie/tree2yaml/model"
 	"gopkg.in/yaml.v2"
@@ -44,4 +45,42 @@ func PrintArchive(archive map[uint64]model.FileArchive) {
 	}
 
 	fmt.Println(string(out))
+}
+
+func PrintAllFiles(filelist *model.FileTree) {
+	path := filelist.RootDir
+
+	for _, file := range filelist.Tree.Files {
+		file.Path = path
+		fmt.Println(file.Name)
+	}
+
+	var rootFolders = filelist.Tree.Folders
+	for _, folder := range rootFolders {
+
+		subPath := path + "/" + folder.Name + "/"
+		traverseFiles(folder.Files, subPath)
+
+		for _, nextFolder := range folder.Folders {
+			subPath = folder.Name + "/" + nextFolder.Name + "/"
+			traverseFolders(nextFolder, subPath)
+		}
+	}
+}
+
+func traverseFiles(files []*model.File, path string) {
+	for _, file := range files {
+		file.Path += path
+		file.Path = strings.TrimSuffix(file.Path, "/")
+		fmt.Println(file.Name, " ", file.Path)
+	}
+}
+
+func traverseFolders(folders *model.Folder, path string) {
+	traverseFiles(folders.Files, path)
+
+	for _, folder := range folders.Folders {
+		subPath := path + folder.Name + "/"
+		traverseFolders(folder, subPath)
+	}
 }
