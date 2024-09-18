0d5e182e2a0b8cc758bfb14b876083f7c8c5d6bf
Rickie Karp <rickie.karp@yandex.com>
2024-09-18 18:17:35 +0200

    Add back PrintAllFilesAndSendFileEvents function

--- printer/printer.go
+++ printer/printer.go
@@ -3,7 +3,9 @@ package printer
 import (
 	"fmt"
 	"sort"
+	"strings"
 
+	"git.rickiekarp.net/rickie/tree2yaml/eventsender"
 	"git.rickiekarp.net/rickie/tree2yaml/model"
 	"gopkg.in/yaml.v2"
 )
@@ -44,4 +46,42 @@ func PrintArchive(archive map[uint64]model.FileArchive) {
 	}
 
 	fmt.Println(string(out))
+}
+
+func PrintAllFilesAndSendFileEvents(filelist *model.FileTree) {
+	path := filelist.RootDir
+
+	for _, file := range filelist.Tree.Files {
+		file.Path = path
+		fmt.Println(file.Name)
+	}
+
+	var rootFolders = filelist.Tree.Folders
+	for _, folder := range rootFolders {
+		subPath := folder.Name + "/"
+		traverseFiles(folder.Files, subPath)
+
+		for _, nextFolder := range folder.Folders {
+			subPath = folder.Name + "/" + nextFolder.Name + "/"
+			traverseFolders(nextFolder, subPath)
+		}
+	}
+}
+
+func traverseFiles(files []*model.File, path string) {
+	for _, file := range files {
+		file.Path += path
+		file.Path = strings.TrimSuffix(file.Path, "/")
+		fmt.Println(file.Name, " ", file.Path)
+		eventsender.SendEventForFile(*file)
+	}
+}
+
+func traverseFolders(folders *model.Folder, path string) {
+	traverseFiles(folders.Files, path)
+
+	for _, folder := range folders.Folders {
+		subPath := path + folder.Name + "/"
+		traverseFolders(folder, subPath)
+	}
 }
