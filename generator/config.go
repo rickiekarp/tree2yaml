f86ce794adc2e31a7a03ee2e26a2f7d66ecf405d
Rickie Karp <rickie.karp@yandex.com>
2025-07-11 20:46:53 +0200

    Remove Archive GenerationType

--- generator/config.go
+++ generator/config.go
@@ -4,15 +4,12 @@ type GenerationType int32
 
 const (
 	Metadata GenerationType = iota
-	Archive
 )
 
 func (s GenerationType) String() string {
 	switch s {
 	case Metadata:
 		return "meta"
-	case Archive:
-		return "archive"
 	}
 	return "unknown"
 }
--- model/comparator.go
+++ model/comparator.go
@@ -4,5 +4,5 @@ func isEqualFile(a, b *File) bool {
 	return a != nil && b != nil &&
 		a.Name == b.Name &&
 		a.Size == b.Size &&
-		a.LastModified.UTC() == b.LastModified.UTC()
+		a.LastModified.UTC().Equal(b.LastModified.UTC())
 }
